0000500e308ce31428af95a6a4c10bdf89f44aea
AuxiBot <77000200+AuxiBot@users.noreply.github.com>
2021-01-05 11:37:03 +0100

    Add files via upload

---
+++ huts.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__, template_folder="templates")
+
+bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train(
+    "/Users/sammi/Videos/Auxi/custom/custom.yml"
+)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+ 
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(bot.get_response(userText))
+ 
+ 
+if __name__ == "__main__":
+    app.run()
